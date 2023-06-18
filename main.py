c456b3d54fc0e8221211929f65b8127bd6c034a1
Ojaswa Sharma <93905595+Sowoul@users.noreply.github.com>
2023-06-18 19:27:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import os
+import uuid
 from pytube import YouTube
 from flask import Flask, request, send_file
-import os
+
 app = Flask(__name__)
 
 
@@ -11,8 +13,15 @@ def strm():
         print(link)
         obj = YouTube(link)
         yt = obj.streams.get_lowest_resolution()
-        yt.download('static')
-        return send_file(f"{os.getcwd()}\static\{obj.title}.mp4", as_attachment=True)
+
+        unique_filename = str(uuid.uuid4()) + '.mp4'
+
+        temp_directory = '/tmp'
+        temp_file = os.path.join(temp_directory, unique_filename)
+
+        yt.download(output_path=temp_directory, filename=unique_filename)
+
+        return send_file(temp_file, as_attachment=True)
     except Exception as e:
         return f'Error: {str(e)}'
 
