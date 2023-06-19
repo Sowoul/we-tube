77582cc3ead75996365361faba2d98509b7c800f
Ojaswa Sharma <93905595+Sowoul@users.noreply.github.com>
2023-06-19 14:25:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def strm():
 
         yt.download(output_path=temp_directory, filename=unique_filename)
 
-        return send_file(temp_file, as_attachment=True)
+        return send_file(temp_file, as_attachment=True,attachment_filename=obj.title)
     except Exception as e:
         return f'Error: {str(e)}'
 
