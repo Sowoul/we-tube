f6b99a2865bb76cfa6b06ca093f9132985e939e1
Ojaswa Sharma <93905595+Sowoul@users.noreply.github.com>
2023-06-19 10:48:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/home')
 def hom():
-    return render_template('home.html')
+    return render_template('index.html')
 @app.route('/')
 def strm():
     link = request.args.get('link')
