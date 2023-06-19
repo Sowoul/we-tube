c05979682d6ac62a517ee670631a0434bfcb61b1
Ojaswa Sharma <93905595+Sowoul@users.noreply.github.com>
2023-06-19 14:16:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from pytube import YouTube
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        link = request.form['link']
+        try:
+            print(link)
+            obj = YouTube(link)
+            yt = obj.streams.get_lowest_resolution()
+            yt.download()
+            return f'<b>Successfully downloaded:</b> {obj.title}'
+        except Exception as e:
+            return f'Error: {str(e)}'
 
-@app.route('/')
-def strm():
-    link = request.args.get('link')
-    try:
-        print(link)
-        obj = YouTube(link)
-        yt = obj.streams.get_lowest_resolution()
-        yt.download()
-        return f'<b>Succesfully downloaded : </b>{obj.title}'
-    except Exception as e:
-        return f'Error: {str(e)}'
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(port=8080)
 
 
 if __name__ == '__main__':
